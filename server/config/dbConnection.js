dff5a9972b7bce3d90f92be85c3209e22a6bc0bd
Ghub09 <ghub02809@gmail.com>
2025-06-30 05:11:29 +0500

    Update dbConnection.js update it remove console.log

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,23 +1,9 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-  console.log(
-    "MONGODB_CONNECTION_STRING",
-    process.env.MONGODB_CONNECTION_STRING
-  );
-  mongoose
-    .connect(process.env.MONGODB_CONNECTION_STRING, {
-      dbName: "BitEx-Crypto-Trading",
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    .then(() => {
-      console.log("Database Has Been Successfully Connected");
-    })
-    .catch((err) => {
-      console.log(
-        "Some error occured while connecting to database",
-        err.message
-      );
-    });
+  return mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+    dbName: "BitEx-Crypto-Trading",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
